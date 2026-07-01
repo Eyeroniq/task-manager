5e438e182373549c573b5d2ca5ded0a90b9cb53e
Eyeroniq <dheeraj9890552178@gmail.com>
2026-07-01 16:30:14 +0530

    chore:frontend dashboard and activities

--- frontend/src/types/activity.ts
+++ frontend/src/types/activity.ts
@@ -1,15 +1,21 @@
 export interface Activity {
   id: number;
   action: string;
+  details: string;
   createdAt: string;
 
   user: {
     id: number;
     fullName: string;
+    email: string;
   };
 
   task: {
     id: number;
     title: string;
-  };
+    description: string;
+    status: string;
+    priority: string;
+    dueDate: string;
+  } | null;
 }
